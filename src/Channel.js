502a095c25e3ae9ff670d9ee9d32d02491d19a9e
Tasso Evangelista <tasso@tassoevan.me>
2018-04-30 15:40:11 -0300

    Update channel appearance in list

--- src/Channel.js
+++ src/Channel.js
@@ -1,20 +1,24 @@
 import React, { Component } from 'react';
 import './Channel.css';
+import ellipsisIcon from './assets/icons/ellipsis-v.svg';
 
 export class Channel extends Component {
   render() {
     return (
-      <div className="Channel">
-        <div className="Channel__icon">G</div>
+      <button className="Channel">
+        <div className="Channel__icon Channel__icon--1">G</div>
         <div className="Channel__description">
           <div className="Channel__name">general</div>
           <div className="Channel__last-message">ray.munoz: Lorem ipsum dolor</div>
         </div>
         <div className="Channel__stats">
           <div className="Channel__last-message-time-since">2h</div>
-          <div className="Channel__message-count Sidebar__list-counter">2</div>
+          <div className="Channel__unread-messages-count Sidebar__list-counter">2</div>
         </div>
-      </div>
+        <button className="Channel__more" aria-label="More">
+          <img src={ellipsisIcon} alt="More" />
+        </button>
+      </button>
     );
   }
 }
