6ba93c4c087a6c958f476ada1985b8310ecd169b
Tasso Evangelista <tasso@tassoevan.me>
2018-05-01 14:24:43 -0300

    Fix actions

--- src/actions.js
+++ src/actions.js
@@ -3,16 +3,16 @@ export const SET_SIDEBAR_AVATAR_VISIBILITY = 'SET_SIDEBAR_AVATAR_VISIBILITY';
 export const SET_SIDEBAR_SORTING = 'SET_SIDEBAR_SORTING';
 
 export const setSidebarViewMode = viewMode => ({
-  action: SET_SIDEBAR_VIEWMODE,
+  type: SET_SIDEBAR_VIEWMODE,
   viewMode
 });
 
 export const setSidebarAvatarVisibility = show => ({
-  action: SET_SIDEBAR_VIEWMODE,
+  type: SET_SIDEBAR_VIEWMODE,
   show: !!show
 });
 
 export const setSidebarSorting = sort => ({
-  action: SET_SIDEBAR_VIEWMODE,
+  type: SET_SIDEBAR_VIEWMODE,
   sort
 });
