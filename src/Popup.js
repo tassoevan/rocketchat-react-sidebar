ee9e5ac9a6a3c29b21937d1d661dcaae97a6dad1
Tasso Evangelista <tasso@tassoevan.me>
2018-05-01 19:02:27 -0300

    Fix popup events

--- src/Popup.js
+++ src/Popup.js
@@ -1,17 +1,20 @@
-import React from 'react';
+import React, { Children } from 'react';
 import './Popup.css';
 
 export const Popup = ({ children, visible, onDismiss, ...props }) => visible ? (
-  <div className="Popup__wrapper" onClick={onDismiss}>
-    <ul className="Popup" {...props}>
-      {children}
+  <div className="Popup__wrapper" onClick={e => onDismiss && onDismiss()}>
+    <ul className="Popup" onClick={e => e.stopPropagation()} {...props}>
+      {Children.map(children, child => React.cloneElement(child, { onDismiss }))}
     </ul>
   </div>
 ) : null;
 
-export const Item = ({ children, icon, selected, ...props }) => (
+export const Item = ({ children, icon, selected, onClick, onDismiss, ...props }) => (
   <li className={`Popup__item${selected ? ' Popup__item--selected' : ''}`}>
-    <button className="Popup__item-button" {...props}>
+    <button className="Popup__item-button" onClick={e => {
+      onDismiss && onDismiss();
+      onClick && onClick(e);
+    }} {...props}>
       {children}
     </button>
   </li>
