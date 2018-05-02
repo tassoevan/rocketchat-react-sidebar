a8e0148ff79994d14390b5cb2370e3263ef1bf1d
Tasso Evangelista <tasso@tassoevan.me>
2018-05-02 01:22:17 -0300

    Purify Channel component

--- src/sidebar/Channel.js
+++ src/sidebar/Channel.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { TextAvatar } from '../Avatar';
 import Icon from '../Icon';
 
@@ -28,44 +28,39 @@ const ChannelAvatar = ({ name, visible }) => {
   );
 };
 
-export class Channel extends Component {
-  render() {
-    const {
-      name,
-      unreadMessages,
-      lastMessage,
-      viewMode,
-      showAvatar
-    } = this.props;
+export const Channel = ({
+  name,
+  unreadMessages,
+  lastMessage,
+  viewMode,
+  showAvatar
+}) => (
+  <div className={`Channel Channel--${viewMode}${unreadMessages > 0 ? ' Channel--has-unread-messages' : ''}`}>
+    <button className="Channel__body">
+      <ChannelAvatar name={name} visible={showAvatar} />
 
-    return (
-      <div className={`Channel Channel--${viewMode}${unreadMessages > 0 ? ' Channel--has-unread-messages' : ''}`}>
-        <button className="Channel__body">
-         <ChannelAvatar name={name} visible={showAvatar} />
-          <div className="Channel__description">
-            <div className="Channel__name">{name}</div>
-            {lastMessage ? (
-              <div className="Channel__last-message">{lastMessage.user}: {lastMessage.message}</div>
-            ) : (
-              <div className="Channel__last-message Channel__last-message--none">No messages yet</div>
-            )}
-          </div>
-
-          <div className="Channel__stats">
-            <div className="Channel__last-message-time-since">2h</div>
-            {unreadMessages > 0 ? (
-              <div className="Channel__unread-messages-count Sidebar__list-counter">{unreadMessages}</div>
-            ) : null}
-          </div>
-        </button>
-
-        <button className="Channel__more" aria-label="More">
-          <Icon name="ellipsis" />
-        </button>
+      <div className="Channel__description">
+        <div className="Channel__name">{name}</div>
+        {lastMessage ? (
+          <div className="Channel__last-message">{lastMessage.user}: {lastMessage.message}</div>
+        ) : (
+          <div className="Channel__last-message Channel__last-message--none">No messages yet</div>
+        )}
       </div>
-    );
-  }
-}
+
+      <div className="Channel__stats">
+        <div className="Channel__last-message-time-since">2h</div>
+        {unreadMessages > 0 ? (
+          <div className="Channel__unread-messages-count Sidebar__list-counter">{unreadMessages}</div>
+        ) : null}
+      </div>
+    </button>
+
+    <button className="Channel__more" aria-label="More">
+      <Icon name="ellipsis" />
+    </button>
+  </div>
+);
 
 export const ChannelNotFound = ({ showAvatar }) => (
   <div className="Channel Channel--not-found">
